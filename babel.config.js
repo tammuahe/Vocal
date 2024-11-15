d4f8988546bff660af388c8bbdbc229475b82c62
tammuahe <137916916+tammuahe@users.noreply.github.com>
2024-11-15 23:50:21 +0700

    fix nativewind

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'], // Only one 'presets' key
-    plugins: ['nativewind/babel'], // Includes NativeWind plugin
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
   };
 };

3ef93cc84c010e48f4ffd535fb5c2f6dfc874e5e
tammuahe <tammuahe2004@gmail.com>
2024-11-15 17:41:19 +0700

    install expo router and nativewind

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = function (api) {
+    api.cache(true);
+    return {
+      presets: ['babel-preset-expo'],
+    };
+  };
+  
+  /** @type {import('tailwindcss').Config} */
+module.exports = {
+    // NOTE: Update this to include the paths to all of your component files.
+    content: ["./app/**/*.{js,jsx,ts,tsx}"],
+    presets: [require("nativewind/preset")],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }

4b55ec6865b0459213edded2e9c78527c9f6a8c5
nguyenhuycuong1 <nhclnhc@gmail.com>
2024-12-06 08:36:39 +0700

    feat: list friends + search friend

--- app/(app)/listFriends.jsx
+++ app/(app)/listFriends.jsx
@@ -1,5 +1,5 @@
 import React, { Component, useEffect, useState } from 'react'
-import { Alert, Button, SafeAreaView, Text, View, TextInput } from 'react-native'
+import { Alert, Button, SafeAreaView, Text, View, TextInput, FlatList } from 'react-native'
 import { useAuth } from "@/context/authContext";
 import SharedLayout from '@/components/SharedLayout'
 import UserAddIcon from '../../assets/icons/user-add-svgrepo-com.svg'
@@ -11,17 +11,17 @@ import FriendItem from '../../components/FriendItem';
 export default function ListFriends() {
     const router = useRouter();
     const  { logout, user } = useAuth();
-    const [friends, setFriends] = useState([])
+    const [friends, setFriends] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
     const getListFriends = async () => {
         try {
             const {data, error} = await supabase
             .from('friends')
-            .select('*')
-            .eq('relation_id', user.id);
+            .select('*').eq('status', 'friend')
             if(error) {
                 console.error('Supabase error: ', error);
             }else {
-                console.log(data);
+                setFriends(data);
             }
         }catch(err) {
             console.error('Unexpected error:', err);
@@ -41,14 +41,52 @@ export default function ListFriends() {
         router.push('/(app)/addFriend')
     }
 
+
+    const searchFriendByUsername = async (user_name) => {
+        if (user_name !== '') {
+            const { data, error } = await supabase
+                .from('profiles')
+                .select('uuid')
+                .like('user_name', `%${user_name}%`); 
+            
+            if (data) {
+                const searchFriends = data
+                .filter(item => item.uuid != user.id)
+                .map(item => ({
+                    smaller_id: item.uuid
+                }));
+                setFriends(searchFriends);
+            } else {
+                console.error('Supabase error: ', error);
+            }
+        } else {
+            await getListFriends(); 
+        }
+    };
+
+    const handleChangeSearchInput = async (value) => {
+        setSearchValue(value);
+        await searchFriendByUsername(value); 
+    };
+
     return (
         <SharedLayout headerTitle="Bạn bè" leftIcon={<UserAddIcon onPress={navigateToAddFriendScreen}/>}>
             <View>
-                <TextInput className='w-full bg-white rounded-full px-6 py-4 mt-2 text-base' placeholder='Tìm bạn bè'/>
+                <TextInput className='w-full bg-white rounded-full px-6 py-4 mt-2 text-base' placeholder='Tìm bạn bè'
+                    value={searchValue} onChangeText={handleChangeSearchInput}
+                />
+                {searchValue == '' && 
+                    <View className='py-4'>
+                        <Text className='text-white text-3xl font-bold'>{friends?.length} bạn bè</Text>
+                    </View>
+                }
                 <View className='py-4'>
-                    <FriendItem className="mb-2" infor={{userName: 'Nguyễn Huy Cường', matualFriend: 20}}/>
-                    <FriendItem  className="mb-2" infor={{userName: 'Nguyễn Huy Cường', matualFriend: 20}}/>
-                    <FriendItem  className="mb-2" infor={{userName: 'Nguyễn Huy Cường', matualFriend: 20}}/>
+                    <FlatList 
+                        data={friends}
+                        keyExtractor={item => item.relation_id}
+                        renderItem={({item}) => <FriendItem className="mb-2" infor={item}/>}
+                    />
+                    
                 </View>
 
                 <Text>hleoeo</Text>
--- components/FriendItem.jsx
+++ components/FriendItem.jsx
@@ -2,8 +2,39 @@ import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { Image } from 'expo-image';
 import DotMenuIcon from '../assets/icons/dot-menu-more-2-svgrepo-com.svg'
+import { useAuth } from "@/context/authContext";
+import { useEffect, useState } from "react";
+import { supabase } from '@/lib/supabase'
+
+
 
 export default function FriendItem({infor ,...res}) {
+    const {user} = useAuth();
+    const [friend, setFriend] = useState({});
+
+    const getInforFriend = async (friend_id) => {
+        const {data, error} = await supabase.
+        from('profiles').select('*').eq('uuid',friend_id);
+        // console.log(data);
+        if(data) {
+            setFriend(data[0]);
+        }else {
+            console.error('Supabase error: ',error);
+        }
+    }
+    
+    useEffect(()=>{
+        const friend_id = infor.smaller_id != user.id ? infor.smaller_id : infor.bigger_id;
+        if(friend_id) {
+            const fetchData = async () => {
+                await getInforFriend(friend_id);
+            }
+            fetchData();
+        }else {
+            console.error('no friend_id')
+        }
+    }, [user])
+
     return (
         <View {...res}>
         <View className="bg-white w-full h-[60px] rounded-xl px-2 flex-row items-center" >
@@ -14,8 +45,8 @@ export default function FriendItem({infor ,...res}) {
                 placeholder={require('../assets/images/default-conversation.png')}
             />
             <View className="ml-1">
-                <Text className="text-lg font-semibold"> {infor.userName}</Text>
-                <Text className="text-[#A9A9A9]"> {infor.matualFriend} bạn chung</Text>
+                <Text className="text-lg font-semibold"> {friend.user_name}</Text>
+                {/* <Text className="text-[#A9A9A9]"> {infor.matualFriend} bạn chung</Text> */}
             </View>
             <TouchableOpacity className="ml-auto mr-1">
                 <DotMenuIcon/>

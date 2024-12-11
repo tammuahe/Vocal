84d24b1fb9cb91df850f06edf54df443c9a302c7
tammuahe <tammuahe2004@gmail.com>
2024-12-11 16:40:24 +0700

    chore(listFriends): format code

--- app/(app)/addFriend.jsx
+++ app/(app)/addFriend.jsx
@@ -1,79 +1,89 @@
-import {View, Text,TextInput, FlatList} from 'react-native';
-import SharedLayout from '@/components/SharedLayout';
-import ReturnBackIcon from '../../assets/icons/return-back-button-svgrepo-com-light.svg';
-import { useRouter } from 'expo-router';
-import { supabase } from '@/lib/supabase'
-import { useEffect, useState } from 'react';
+import { View, Text, TextInput, FlatList } from "react-native";
+import SharedLayout from "@/components/SharedLayout";
+import ReturnBackIcon from "../../assets/icons/return-back-button-svgrepo-com-light.svg";
+import { useRouter } from "expo-router";
+import { supabase } from "@/lib/supabase";
+import { useEffect, useState } from "react";
 import { useAuth } from "@/context/authContext";
-import FriendItem from '../../components/FriendItem';
-
+import FriendItem from "../../components/FriendItem";
 
 export default function AddFriend() {
-    const router = useRouter();
-    const  { user } = useAuth();
-    const [friendInvitations, setFriendInvitations] = useState([]);
+  const router = useRouter();
+  const { user } = useAuth();
+  const [friendInvitations, setFriendInvitations] = useState([]);
 
-    const getListFriendInvitations = async () => {
-        const {data, error} = await supabase
-                                .from('friends')
-                                .select('*')
-                                .eq('status', 'pending')
-                                .neq('sender_id', user.id);
-        if(error){
-            console.error('Supabase error: ', error);
-        }else {
-            setFriendInvitations(data)
-        }
+  const getListFriendInvitations = async () => {
+    const { data, error } = await supabase
+      .from("friends")
+      .select("*")
+      .eq("status", "pending")
+      .neq("sender_id", user.id);
+    if (error) {
+      console.error("Supabase error: ", error);
+    } else {
+      setFriendInvitations(data);
     }
+  };
 
-    const acceptInvitatinon = async (relation_id) => {
-        console.log(relation_id);
-        if(relation_id) {
-            const {error} = await supabase.from('friends').update({status: 'friend'}).eq('relation_id', relation_id)
-            if(error) {
-                console.error('Supabase error: ',error)
-            }else {
-                await getListFriendInvitations();
-            }
-        }else {
-            console.error('no relation_id');
-        }
+  const acceptInvitatinon = async (relation_id) => {
+    console.log(relation_id);
+    if (relation_id) {
+      const { error } = await supabase
+        .from("friends")
+        .update({ status: "friend" })
+        .eq("relation_id", relation_id);
+      if (error) {
+        console.error("Supabase error: ", error);
+      } else {
+        await getListFriendInvitations();
+      }
+    } else {
+      console.error("no relation_id");
     }
+  };
 
-    useEffect(() => {
-        const fetchData = async () => {
-            await getListFriendInvitations();
-        }
-        if(user) {
-            fetchData();
-        }
-    }, [user])
+  useEffect(() => {
+    const fetchData = async () => {
+      await getListFriendInvitations();
+    };
+    if (user) {
+      fetchData();
+    }
+  }, [user]);
 
-    return (
-        <SharedLayout headerTitle="Bạn bè" leftIcon={<ReturnBackIcon onPress={() => router.push('/(app)/listFriends')}/>}>
-            
-            <View className='flex-1'>
-                <TextInput className='w-full bg-white rounded-full px-6 py-4 mt-2 text-base' placeholder='Tìm kiếm'/>
-                {friendInvitations.length > 0 &&
-                    (<View>
-                       <Text className='py-4 text-3xl text-white font-bold'>{friendInvitations.length} lời mời kết bạn mới</Text>
-    
-                        <FlatList 
-                            data={friendInvitations}
-                            keyExtractor={(item,index) => item.smaller_id}
-                            renderItem={({item}) => <FriendItem 
-                                                        className="mb-2" 
-                                                        infor={item} 
-                                                        type='pending_friend' 
-                                                        onAcceptFriend={() => acceptInvitatinon(item.relation_id)}
-                                                    />
-                                        }
-                        />
-                            
-                    </View>
-                    )
-                }
-            </View>
-        </SharedLayout>
-    )
+  return (
+    <SharedLayout
+      headerTitle="Bạn bè"
+      leftIcon={
+        <ReturnBackIcon onPress={() => router.push("/(app)/listFriends")} />
+      }
+    >
+      <View className="flex-1">
+        <TextInput
+          className="w-full bg-white rounded-full px-6 py-4 mt-2 text-base"
+          placeholder="Tìm kiếm"
+        />
+        {friendInvitations.length > 0 && (
+          <View>
+            <Text className="py-4 text-3xl text-white font-bold">
+              {friendInvitations.length} lời mời kết bạn mới
+            </Text>
+
+            <FlatList
+              data={friendInvitations}
+              keyExtractor={(item, index) => item.smaller_id}
+              renderItem={({ item }) => (
+                <FriendItem
+                  className="mb-2"
+                  infor={item}
+                  type="pending_friend"
+                  onAcceptFriend={() => acceptInvitatinon(item.relation_id)}
+                />
+              )}
+            />
+          </View>
+        )}
+      </View>
+    </SharedLayout>
+  );
 }

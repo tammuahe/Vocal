567a3b9e43a118b42cf8ef2a5fe86f2c0f8ef59f
tammuahe <tammuahe2004@gmail.com>
2024-12-11 16:38:59 +0700

    feat(home): add create conversation bottom sheet

--- app/(app)/_layout.jsx
+++ app/(app)/_layout.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import { Stack } from 'expo-router'
-import { AuthContextProvider } from '@/context/authContext';
-import HomeHeader from '@/components/HomeHeader.jsx';
-import { MenuProvider } from 'react-native-popup-menu';
+import React from "react";
+import { Stack } from "expo-router";
+import { AuthContextProvider } from "@/context/authContext";
+import { MenuProvider } from "react-native-popup-menu";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function _layout() {
-    return (
-      <AuthContextProvider>
-        <MenuProvider>
+  return (
+    
+    <AuthContextProvider>
+      <MenuProvider>
+      <GestureHandlerRootView className="flex-1">
+        <SafeAreaProvider>
           <Stack>
-            <Stack.Screen name='home' options={{headerShown: false}}/>
-            <Stack.Screen name='addFriend' options={{headerShown: false} }/>
-            <Stack.Screen name='listFriends' options={{headerShown: false} }/>
+            <Stack.Screen name="home" options={{ headerShown: false }} />
+            <Stack.Screen name="addFriend" options={{ headerShown: false }} />
+            <Stack.Screen name="listFriends" options={{ headerShown: false }} />
           </Stack>
-        </MenuProvider>
-      </AuthContextProvider>
-    ) 
+        </SafeAreaProvider>
+        </GestureHandlerRootView>
+      </MenuProvider>
+    </AuthContextProvider>
+
+  );
 }
